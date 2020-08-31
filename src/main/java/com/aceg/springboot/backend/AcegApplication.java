1deacaa94a740cf183680c86d44dc424ce6e7777
Edgar Rangel - Neoris <e-edgarrangel@neoris.com>
2020-08-31 18:59:05 -0500

    log4j2 configurado, sonar terminado

--- src/main/java/com/aceg/springboot/backend/AcegApplication.java
+++ src/main/java/com/aceg/springboot/backend/AcegApplication.java
@@ -1,13 +1,24 @@
 package com.aceg.springboot.backend;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AcegApplication {
 
+	private static final Logger LOGGER = LogManager.getLogger(AcegApplication.class);
+
 	public static void main(String[] args) {
+		
 		SpringApplication.run(AcegApplication.class, args);
+		
+        LOGGER.debug("Debug Message!");
+        LOGGER.info("Info Message!");
+        LOGGER.warn("Warn Message!");
+        LOGGER.error("Error Message!");
+        LOGGER.fatal("Fatal Message!");
 	}
 
 }
